a806c9e455a4cd2b9994422119f6313f12d3d28d
Hassan <hassanett21@gmail.com>
2025-04-10 01:01:31 +0100

    push

--- philo.c
+++ philo.c
@@ -1,11 +1,11 @@
 #include "philo.h"
 
-void set_inputs(t_input *input, int c, int i)
+int set_inputs(t_input *input, int c, int i)
 {
     if(i == 1)
     {
         if(c > 200)
-            return (write(2, "Error: number of philo must be under 200...", 43), exit(1));
+            return (write(2, "Error: number of philo must be under 200...", 43), 3);
         input->number_of_philosophers = c;
     }
     if(i == 2)
@@ -16,9 +16,12 @@ void set_inputs(t_input *input, int c, int i)
         input->time_to_sleep = c;
     if(i == 5)
         input->number_of_times_each_philosopher_must_eat = c;
+    else
+        input->number_of_times_each_philosopher_must_eat = -1;
+    return (0);
 }
 
-void check_param(int ac, char **av, t_input *input)
+int check_param(int ac, char **av, t_input *input)
 {
     int i;
     long c;
@@ -28,20 +31,27 @@ void check_param(int ac, char **av, t_input *input)
     {
         c = ft_atoi((const char *)av[i]);
         if(c == 0)
-            return (write(2, "Error: input must be between 1 and 2147483647...", 49), exit(1));
+            return (write(2, "Error: input must be between 1 and 2147483647...", 49), 1);
         if(c == -1)
-            return (write(2, "Error: input must be positive...", 33), exit(1));
-        set_inputs(input, c, i);
+            return (write(2, "Error: input must be positive...", 33), 2);
+        int k = set_inputs(input, c, i);
+        if (k != 0)
+            return (3);
         i++;
     }
+    return (0);
 }
-
+// parsing check only numbers, if not return error;
 int main(int ac, char **av)
 {
     t_input input;
+    int e;
 
     if(!(ac == 5 || ac == 6))
         return (write(2, "Error: invalid number of parameter...", 38), 1);
-    check_param(ac, av, &input);
+    parsing(av);
+    e = check_param(ac, av, &input);
+    if(e > 0)
+        return 1;
     printf("%d | %d | %d | %d | %d | ", input.number_of_philosophers, input.time_to_die, input.time_to_eat, input.time_to_sleep, input.number_of_times_each_philosopher_must_eat);
 }
